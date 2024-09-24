72e454cc66576a8827ef903303469a04398a9957
CROUILLERE Kévin <kevin.crouillere@softeam.fr>
2024-09-24 12:54:30 +0200

    Update, lessons 2

---
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, AsyncPipe, UpperCasePipe, DatePipe } from '@angular/common';
+import { FormBuilder, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { from, Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { FaceSnap } from '../models/face-snap';
+import { FaceSnapsService } from '../services/face-snaps.service';
+
+@Component({
+  selector: 'app-new-face-snap',
+  standalone: true,
+  imports: [
+      ReactiveFormsModule,
+      CommonModule,
+      UpperCasePipe,
+      DatePipe
+  ],
+  templateUrl: './new-face-snap.component.html',
+  styleUrl: './new-face-snap.component.scss'
+})
+export class NewFaceSnapComponent implements OnInit {
+
+      snapForm!: FormGroup;
+      faceSnapPreview$!: Observable<FaceSnap>;
+      urlRegex!: RegExp;
+
+      constructor(private formBuilder: FormBuilder,
+                  private faceSnapsService: FaceSnapsService,
+                  private router: Router) { }
+
+      ngOnInit(): void {
+            this.urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
+
+            this.snapForm = this.formBuilder.group({
+                title: [null, [Validators.required]],
+                description: [null, [Validators.required]],
+                imageUrl: [null, [Validators.required, Validators.pattern(this.urlRegex)]],
+                location: [null]
+            }, {
+                updateOn: 'blur'
+            });
+
+            this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+                map(formValue => ({
+                    ...formValue,
+                    createdAt: new Date(),
+                    snaps: 0,
+                    id: 0
+                }))
+            );
+      }
+
+      onSubmitForm() {
+          this.faceSnapsService.addFaceSnap(this.snapForm.value);
+          this.router.navigateByUrl('/facesnaps');
+      }
+
+}
